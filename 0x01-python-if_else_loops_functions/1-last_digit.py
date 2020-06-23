27281b3fb25b85503a364723b4f6a4615be72ef1
nedhir6 <nedhirb@gmail.com>
2020-06-23 11:40:20 +0100

    typo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ mod = number % 10
 if number < 0:
     mod = number % -10
 if mod == 0:
-    print("last digit of {} is {} and is 0".format(number, mod))
+    print("Last digit of {} is {} and is 0".format(number, mod))
 elif mod > 5:
-    print("last digit of {} is {} and is greater than 5".format(number, mod))
+    print("Last digit of {} is {} and is greater than 5".format(number, mod))
 elif mod < 6:
-    print("last digit of {} is {} and is less than 6 and not 0".format(number, mod))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, mod))
