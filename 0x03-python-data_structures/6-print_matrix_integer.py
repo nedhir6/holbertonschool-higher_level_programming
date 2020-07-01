ce868e1d3f282dd5ac184c5d92f935e86ffc8669
nedhir6 <nedhirb@gmail.com>
2020-07-01 11:57:54 +0100

    handle empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end="")
-            if j < (len(matrix[i]) - 1):
-                print(" ", end="")
-            else:
-                print("")
+    if matrix == [[]]:
+        return
+    else:
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j < (len(matrix[i]) - 1):
+                    print(" ", end="")
+                else:
+                    print("")
