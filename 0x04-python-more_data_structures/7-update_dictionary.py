e279b657d532e24592b6d1362c11f6149797b891
nedhir6 <nedhirb@gmail.com>
2020-07-18 14:21:18 +0100

    fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    flag = 0
     for i in a_dictionary:
         if i == key:
             a_dictionary[key] = value
             flag = 1
     if flag is not 1:
         a_dictionary[key] = value
+    return(a_dictionary)
