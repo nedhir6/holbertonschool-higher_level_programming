c978dbe905c011321c331d4d0dc92555906ffd1c
nedhir6 <nedhirb@gmail.com>
2020-07-18 11:20:32 +0100

    p

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    list = my_list.copy()
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            list[i] = replace
-        return list
+        return [(num if num is not search else replace) for num in my_list]
