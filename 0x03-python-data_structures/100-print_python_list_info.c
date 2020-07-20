b31cc80fb1360e91f1b16ab08a013239486a4362
nedhir6 <nedhirb@gmail.com>
2020-07-21 00:09:30 +0100

    100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -13,7 +13,7 @@ Py_ssize_t psize;
 
 psize = PyList_Size(p);
 printf("[*] Size of the Python List = %li\n", psize);
-printf("[*] Allocated = %li\n", (PyListObject *)p-> allocated);
+printf("[*] Allocated = %li\n", (((PyListObject *)p)-> allocated));
 for (i = 0; i < psize; i++)
 {
 	po = PyList_GetItem(p, i);
