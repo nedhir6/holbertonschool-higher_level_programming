4246808316aa73da3bf41ac2ef51c7b306f726d7
nedhir6 <nedhirb@gmail.com>
2020-07-20 18:00:58 +0100

    handling empty list case

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if my_list == []:
         return 0
     return sum(list((a * b) for a, b in my_list)) / sum(c[1] for c in my_list)
