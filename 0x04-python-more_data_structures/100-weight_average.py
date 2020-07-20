34da8aae13d8e301703524617acf622b11fa1e95
nedhir6 <nedhirb@gmail.com>
2020-07-20 17:56:25 +0100

    typo fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-
     if a_dictionary is None:
         return 0
-    eturn sum(list((a * b) for a, b in my_list)) / sum(c[1] for c in my_list)
+    return sum(list((a * b) for a, b in my_list)) / sum(c[1] for c in my_list)
