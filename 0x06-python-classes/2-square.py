c05954e4bc3808acf0922b6144b403163896227b
nedhir6 <nedhirb@gmail.com>
2020-07-26 08:10:56 +0100

    if raise

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """size"""
+        if size < 0:
+            raise ValueError ("size must be >= 0")
+        elif type(size) is not int:
+            raise TypeError ("size must be an integer")
+        self.__size = size
+    pass
