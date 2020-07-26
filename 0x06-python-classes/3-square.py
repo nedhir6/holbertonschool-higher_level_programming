cbe9ca5f8071097c34c73f7f2e20592d5c2ba31d
nedhir6 <nedhirb@gmail.com>
2020-07-26 08:35:10 +0100

    __area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size=0):
+        """size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area"""
+        ar = self.__size * self.__size
+        return ar
+    pass
