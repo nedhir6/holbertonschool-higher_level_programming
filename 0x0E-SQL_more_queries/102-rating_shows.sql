0c6e57a69f7b0a6c97f4f810c6b3f204c324eeb4
nedhir6 <nedhirb@gmail.com>
2020-08-04 21:23:39 +0100

    fix

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,4 @@
--- lists all shows by their rating.
+-- lists all shows from the database by their rating.
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
