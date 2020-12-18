e71e9e22af8e5800d4d8f01e457819267ed5ced0
nedhir6 <nedhirb@gmail.com>
2020-12-18 14:48:22 +0100

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  displays all HTTP methods the server will accept.
-curl -sX OPTIONS * $1 -i | grep -i Allow | awk -F " " '{$1="";print $0}' | tail -c +2
+curl -s $1 -I | grep Allow | awk -F " " '{$1="";print $0}' | tail -c +2
